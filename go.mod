b3a329a2c50ac4fe36aedfda445ae65dcba5b2dc
hezhaowu <hezhaowu@kingsoft.com>
2019-08-20 11:23:02 +0800

    save

--- go.mod
+++ go.mod
@@ -1,3 +1,20 @@
-module github.com/obase/obase
+module github.com/obase
 
 go 1.12
+
+require (
+	github.com/obase/api v1.4.1
+	github.com/obase/apix v1.4.1
+	github.com/obase/center v1.4.1
+	github.com/obase/clickhouse v1.4.1
+	github.com/obase/conf v1.4.1
+	github.com/obase/httpgw v1.4.1
+	github.com/obase/httpgw-gin v0.0.6
+	github.com/obase/kafka v1.4.1
+	github.com/obase/log v1.4.1
+	github.com/obase/mongo v1.4.1
+	github.com/obase/mysql v1.4.1
+	github.com/obase/redis v1.4.1
+)
+
+exclude github.com/gin-gonic/gin v1.4.0
--- main.go
+++ main.go
@@ -1,53 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"github.com/obase/obase/cvendor"
-	"github.com/obase/obase/kits"
-	"os"
-	"strings"
-	"time"
+	_ "github.com/obase/api"
+	_ "github.com/obase/apix"
+	_ "github.com/obase/center"
+	_ "github.com/obase/clickhouse"
+	_ "github.com/obase/conf"
+	_ "github.com/obase/httpgw"
+	_ "github.com/obase/httpgw-gin"
+	_ "github.com/obase/kafka"
+	_ "github.com/obase/log"
+	_ "github.com/obase/mongo"
+	_ "github.com/obase/mysql"
+	_ "github.com/obase/redis"
 )
-
-func init() {
-	// 添加子命令
-	all = append(all, &cmd{"cvendor", cvendor.Process, cvendor.Usage})
-}
 
 func main() {
 
-	if len(os.Args) > 1 && os.Args[1] != "help" {
-		for _, c := range all {
-			if c.Name == os.Args[1] {
-				kits.Infof("%v %v start\n", c.Name, os.Args[2:])
-				start := time.Now().UnixNano()
-				c.Process()
-				end := time.Now().UnixNano()
-				kits.Infof("%v %v finish, used time(ms): %v\n", c.Name, os.Args[2:], (end-start)/1000000)
-				return
-			}
-		}
-	}
-	fmt.Fprintln(os.Stderr, usage())
 }
-
-func usage() string {
-	sb := new(strings.Builder)
-	sb.WriteString(fmt.Sprintf("Usage: %s <command> [args...]\n", os.Args[0]))
-	for _, c := range all {
-		sb.WriteString("\n命令: ")
-		sb.WriteString(c.Name)
-		sb.WriteString(", 用法: ")
-		sb.WriteString(c.Usage())
-		sb.WriteString("\n")
-	}
-	return sb.String()
-}
-
-type cmd struct {
-	Name    string
-	Process func()
-	Usage   func() string
-}
-
-var all []*cmd
